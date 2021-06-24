74ddaabb48db181a8ac08e597b6cfb66c8a16761
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2021-06-24 14:25:57 +0100

    add category cards

---
+++ src/components/Category.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+
+import Button from "./Button";
+
+import HeadphoneImg from "../assets/img/headphone.png";
+import SpeakerImg from "../assets/img/speaker.png";
+import EarphoneImg from "../assets/img/earphone.png";
+import OvalBlur from "../assets/svg/oval-blur.svg";
+
+const StyledCategory = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 21.875rem;
+  height: 12.75rem;
+  background: ${({ theme }) => theme.colors.brand.white};
+  border-radius: 8px;
+`;
+
+const CategoryImgWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: -5rem;
+  position: relative;
+`;
+
+const CategoryImg = styled.img`
+  width: 7.6875rem;
+  height: 10rem;
+  object-fit: cover;
+  background: transparent;
+  margin-bottom: 2.25rem;
+`;
+
+const CategoryImgBlur = styled.img`
+  position: absolute;
+  width: 7.6875rem;
+  bottom: 0;
+`;
+
+const CategoryName = styled.h6`
+  font: ${({ theme }) => theme.typography.h6};
+  letter-spacing: 1.3px;
+  font-weight: 700;
+  margin: 0;
+  text-transform: uppercase;
+`;
+
+interface ICategoryProps {
+  name: string;
+}
+
+const Category = ({ name }: ICategoryProps) => {
+  return (
+    <StyledCategory>
+      <CategoryImgWrapper>
+        <CategoryImg src={getImage(name)} alt={name} />
+        <CategoryImgBlur src={OvalBlur} alt="Blur" />
+      </CategoryImgWrapper>
+      <CategoryName>{name}</CategoryName>
+      <Button text="Shop" variant="ghost" />
+    </StyledCategory>
+  );
+};
+
+const getImage = (name: string) => {
+  switch (name) {
+    case "Headphones":
+      return HeadphoneImg;
+    case "Speakers":
+      return SpeakerImg;
+    case "Earphones":
+      return EarphoneImg;
+    default:
+      return "";
+  }
+};
+
+export default Category;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Category from "../components/Category";
+
+const CategoryGroup = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin: 12.5rem 10.3125rem;
+`;
 
 const Landing = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <CategoryGroup>
+        <Category name="Headphones" />
+        <Category name="Speakers" />
+        <Category name="Earphones" />
+      </CategoryGroup>
     </div>
   );
 };
