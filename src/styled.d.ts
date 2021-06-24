56b097f91678335895e9b5ff4cd45e4ca04c2604
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2021-06-24 01:57:37 +0100

    create style theme based on design system

---
+++ src/styled.d.ts
@@ -0,0 +1,32 @@
+// importing the original module declarations
+import "styled-components";
+
+// and extending them
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      brand: {
+        peru: string;
+        black: string;
+        white: string;
+        lotion: string;
+      };
+      neutral: {
+        tangelo: string;
+        white: string;
+        black: string;
+      };
+    };
+    typography: {
+      h1: string;
+      h2: string;
+      h3: string;
+      h4: string;
+      h5: string;
+      h6: string;
+      overline: string;
+      subTitle: string;
+      body: string;
+    };
+  }
+}
---
+++ src/theme/AudiophileTheme.tsx
@@ -0,0 +1,28 @@
+const AudiophileTheme = {
+  colors: {
+    brand: {
+      peru: "#D87D4A",
+      black: "#101010",
+      white: "#F1F1F1",
+      lotion: "#FAFAFA",
+    },
+    neutral: {
+      tangelo: "#FBAF85",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+  },
+  typography: {
+    h1: "56px/58px 'Manrope', sans-serif",
+    h2: "40px/44px 'Manrope', sans-serif",
+    h3: "32px/36px 'Manrope', sans-serif",
+    h4: "28px/38px 'Manrope', sans-serif",
+    h5: "24px/33px 'Manrope', sans-serif",
+    h6: "18px/24px 'Manrope', sans-serif",
+    overline: "14px/19px 'Manrope', sans-serif",
+    subTitle: "13px/25px 'Manrope', sans-serif",
+    body: "15px/25px 'Manrope', sans-serif",
+  },
+};
+
+export default AudiophileTheme;
