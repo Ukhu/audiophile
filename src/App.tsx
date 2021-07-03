4cd640d32ba00e4a29d51d9c6a83e19c1834e810
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2021-07-03 13:28:52 +0100

    add client-side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AudiophileTheme from "./theme/AudiophileTheme";
 
 import Landing from "./pages/Landing";
+import Category from "./pages/Category";
+import Checkout from "./pages/Checkout";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
-    <ThemeProvider theme={AudiophileTheme}>
-      <Landing />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={AudiophileTheme}>
+        <Switch>
+          <Route exact path="/category/:id">
+            <Category />
+          </Route>
+          <Route exact path="/product/:id">
+            <ProductDetail />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 }
 
