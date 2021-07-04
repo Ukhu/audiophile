d36142878fd82cac97c144d21e717ae29f66710d
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2021-07-04 23:09:37 +0100

    add firebase to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import firebase from "firebase";
+import "firebase/firestore";
 
 import AudiophileTheme from "./theme/AudiophileTheme";
 
@@ -8,6 +10,17 @@ import Landing from "./pages/Landing";
 import Category from "./pages/Category";
 import Checkout from "./pages/Checkout";
 import ProductDetail from "./pages/ProductDetail";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
